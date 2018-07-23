bf586e30ffc11b8cdb512d6bd96387bb1b9a23ff
Christiana Vandermale <christianavdm@gmail.com>
2018-07-23 23:41:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+get '/' do 
+  "Hello world" 
+end
+
  get '/reversename/:name' do
    @name = param[:name]
    @name.reverse
